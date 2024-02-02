41543ba4c30789a4b37b744eb35d9d5b7c5e3bdc
shirin <shirinjalilvanf97@gmai.com>
2024-02-02 20:17:02 +0330

    implement new bot

--- birthday-service/src/main/java/ir/reminder/SpringBootReminderApplication.java
+++ birthday-service/src/main/java/ir/reminder/SpringBootReminderApplication.java
@@ -7,18 +7,22 @@ import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
 @SpringBootApplication
 public class SpringBootReminderApplication {
-
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = SpringApplication.run(SpringBootReminderApplication.class, args);
         try {
+            System.getProperties().put("proxySet", "true");
+
+            System.getProperties().put("socksProxyHost", "127.0.0.1");
+
+            System.getProperties().put("socksProxyPort", "2050");
+
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             botsApi.registerBot(ctx.getBean("pizzaBot", AbilityBot.class));
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
-    }
 
+    }
 }

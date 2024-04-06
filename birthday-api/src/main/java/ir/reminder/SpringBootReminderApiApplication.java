d67446f35f9f7b767d8822e9b738c3e075fbaa37
M.Yasinzadeh <m.yasinzdeh@demisco.com>
2024-04-06 16:35:59 +0330

    fixed properties for modules

--- birthday-api/src/main/java/ir/reminder/SpringBootReminderApiApplication.java
+++ birthday-api/src/main/java/ir/reminder/SpringBootReminderApiApplication.java
@@ -3,13 +3,15 @@ package ir.reminder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @SpringBootApplication
 @EnableScheduling
 public class SpringBootReminderApiApplication {
-    public static void main(String[] args) throws TelegramApiException {
+    public static void main(String[] args) {
         SpringApplication.run(SpringBootReminderApiApplication.class, args);
+//            System.getProperties().put("proxySet", "true");
+//            System.getProperties().put("socksProxyHost", "127.0.0.1");
+//            System.getProperties().put("socksProxyPort", "2050");
 
     }
 }
